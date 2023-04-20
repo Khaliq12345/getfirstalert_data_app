6e7bcd9f30aef996e6d55c9765f13b92e9e2d8ee
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-04-20 13:53:28 +0100

    welcome

--- Homepage.py
+++ Homepage.py
@@ -44,8 +44,26 @@ def access_app():
     description= 'Get Accurate and Reliable Data on Influencers and Leads',
     color_name= 'green-70'
     )
-
+    
     st.success('Welcome!')
+
+    st.write("""
+# Welcome to our Streamlit data web app!
+
+Our app is designed to provide you with all the information you need to make informed decisions about B2B companies, influencers, and email verification. With our easy-to-use platform, you can quickly and efficiently gather data on potential partners, influencers, and leads.
+
+Our app is divided into three main sections:
+
+1. **B2B Company Data:** This section allows you to search for and retrieve detailed information on B2B companies, including their industry, size, revenue, and contact information.
+
+2. **Influencer Data:** In this section, you can find data on social media influencers, such as their number of followers, engagement rates, and audience demographics.
+
+3. **Email Verification:** Our email verification tool helps you validate the accuracy of email addresses, ensuring that your messages reach the right people.
+
+Our app is designed with user-friendliness in mind. You don't need any specialized technical skills to use it. Simply navigate to the section you need and enter your search parameters.
+
+So why wait? Start exploring our Streamlit data web app today and take your business to the next level!
+""")
 
     if st.button('Logout!'):
         st.session_state['access'] = False
