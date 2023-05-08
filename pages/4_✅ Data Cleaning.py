13d3f5714717b827c2635d88a1432d889307ddb6
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-05-08 12:40:59 +0100

    index=False

--- pages/4_✅ Data Cleaning.py
+++ pages/4_✅ Data Cleaning.py
@@ -24,7 +24,7 @@ if 'access' not in st.session_state:
 @st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+    return df.to_csv(index=False).encode('utf-8')
 
 def is_valid(address):
     address = str(address)
